c6e40d785cbc0b433d54befc4f453154383eca34
Lau <laura.fabbiano.gomez@hotmail.com>
2024-04-14 22:37:59 +0200

    frontend almost done but something doesnt work

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -14,16 +14,20 @@ def fetch_movies():
     response = requests.get(backend_url)
     return pd.DataFrame(response.json()) if response.status_code == 200 else st.error('Failed to fetch movies from the backend.')
 
+@st.cache_data
 def search(title):
     backend_url = "http://127.0.0.1:8080/search"
-    response = requests.get(backend_url, params={"q" : title})
-    return pd.DataFrame(response.json())
+    response = requests.get(backend_url, params={"q" : title})   
+    movies_found = pd.DataFrame(response.json())
+    return pd.merge(movies_found, fetch_movies(), on='movieId', how='left').rename(columns={'title_x': 'title', 'genres_x': 'genres'})
 
+@st.cache_data
 def posters(tmdb_id):
     backend_url = "http://127.0.0.1:8080/posters"
     response = requests.get(backend_url, params={"tmdb_id" : tmdb_id})
     return response.json()['poster_url']
 
+@st.cache_data
 def details(tmdb_id):
     backend_url = "http://127.0.0.1:8080/details"
     response = requests.get(backend_url, params={"tmdb_id" : tmdb_id})
@@ -31,33 +35,19 @@ def details(tmdb_id):
 
 def recommendations(favorites):
     backend_url = "http://127.0.0.1:8080/recommendations"
-    response = requests.get(backend_url)
-    return pd.DataFrame(response.json()) if response.status_code == 200 else st.error('Failed to get recommendations.')
+    response = requests.get(backend_url, params={"cold_user_movies" : favorites})
+    dt_rec = pd.DataFrame(response.json()) if response.status_code == 200 else st.error('Failed to get recommendations.')
+    return pd.merge(dt_rec, fetch_movies(), on='movieId', how='left')
 
+@st.cache_data
+def get_movie_details(movie_id):
+    movies_list = fetch_movies()
+    return movies_list[movies_list['movieId'] == movie_id]
 
 
 #####################
 # App Design ########
 #####################
-
-# Streamlit UI
-st.title('Movie Recommendation System')
-#movies_df = fetch_movies()
-movies_df = fetch_movies().head(50)
-
-search_query = st.text_input("Search a movie by its title !")
-
-
-#if 'favorites' not in st.session_state:
-#    st.session_state['favorites'] = []
-
-# Vérifie si la clé 'favorites' existe dans st.session_state, sinon initialise à une liste vide
-movies_info = {}  # clé: movieId, valeur: (title, poster_url)
-
-# Remplir le dictionnaire avec les informations des films
-for _, row in movies_df.iterrows():
-    movies_info[row['movieId']] = (row['title'], posters(row['tmdbId']))
-
 
 if 'favorites' not in st.session_state:
     st.session_state['favorites'] = []
@@ -65,34 +55,52 @@ if 'favorites' not in st.session_state:
 with st.sidebar:
     st.title("Favorites")
     for movie_id in st.session_state['favorites']:
-        # Vérifier si l'ID du film est présent dans le dictionnaire
-        if movie_id in movies_info:
-            title, poster_url = movies_info[movie_id]
-            st.markdown(f"### {title}")  # Afficher le titre du film
-            st.image(poster_url, width=150)  # Afficher le poster du film
-        else:
-            st.write(f"Film avec l'ID {movie_id} introuvable")
+        bruh = get_movie_details(movie_id)
+        for index,row in bruh.iterrows() :
+            st.markdown(f"{row['title']}")  # Afficher le titre du film
+            st.image(posters(row["tmdbId"]), width=150)  # Afficher le poster du film
 
+# Streamlit UI
+st.title('Movie Recommendation System')
+
+# Initializes the 'displayed_movies' key in the session state
+if 'displayed_movies' not in st.session_state:
+    st.session_state['displayed_movies'] = fetch_movies().head(50)
+
+search_query = st.text_input("Search a movie by its title!")
+
+col1, col2 = st.columns(2)
+
+# Check if a search has been made and update displayed_movies accordingly
 if search_query:
-    search_results = search(search_query)
-    st.write(search_results)
-else:
-    movies_to_display = 3
+    st.session_state['displayed_movies'] = search(search_query)
 
-    for i in range(0, len(movies_df), movies_to_display):
-        row_data = movies_df.iloc[i:i+movies_to_display]
-        cols = st.columns(movies_to_display)
-        for col, (_, row) in zip(cols, row_data.iterrows()):
-            with col:
-                poster_url = posters(row['tmdbId']) 
-                st.image(poster_url, width=250)
-                # Ajoute l'icône de cœur en haut à droite du poster
-                button_key = f"favorite_button_{row['tmdbId']}"
-                if st.button("❤️", key=button_key):
-                    st.session_state['favorites'].append(row['movieId'])
-                    st.experimental_rerun()
-                expander_key = f"details_expander_{row['tmdbId']}"
-                with st.expander("Afficher les détails", expanded=False):
-                    st.markdown(f"**Genre:** {row['genres']}")
-                    detail = details(row['tmdbId'])
-                    st.write(detail)
+with col1:
+    if st.button("Get recommendations based on your likes"):
+        # If the user clicks the recommendations button, update the displayed_movies
+        st.session_state['displayed_movies'] = recommendations(st.session_state.get('favorites', []))
+with col2:
+    if st.button("Get random movies"):
+        # If the user clicks the random movies button, update the displayed_movies
+        st.session_state['displayed_movies'] = fetch_movies().sample(50)
+
+
+
+# Now, we have a single block of code that displays the movies
+movies_to_display = 3
+for i in range(0, len(st.session_state['displayed_movies']), movies_to_display):
+    row_data = st.session_state['displayed_movies'].iloc[i:i+movies_to_display]
+    cols = st.columns(movies_to_display)
+    for col, (_, row) in zip(cols, row_data.iterrows()):
+        with col:
+            poster_url = posters(row['tmdbId'])
+            st.image(poster_url, width=250)
+            button_key = f"favorite_button_{row['tmdbId']}"
+            if st.button("❤️", key=button_key):
+                st.session_state['favorites'].append(row['movieId'])
+                st.experimental_rerun()
+            expander_key = f"details_expander_{row['tmdbId']}"
+            with st.expander("Show details", expanded=False):
+                st.markdown(f"**Genre:** {row['genres']}")
+                detail = details(row['tmdbId'])
+                st.write(detail)
